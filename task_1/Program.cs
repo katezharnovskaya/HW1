8ea56ccbf988a20eb367ef0f74a6df3a9425b765
Ekaterina <firegirl-83@mail.ru>
2023-01-21 23:27:41 +0300

    add task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b=Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Первое число больше второго" );
+}
+else if (a < b)
+{
+    Console.WriteLine("Первое число меньше второго" );
+}
+else
+{
+    Console.WriteLine("Оба числа равны" );
+
+}
+
+
---
+++ task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c=Convert.ToInt32(Console.ReadLine());
+
+max = a;
+
+if (b > max )
+{
+    max = b;
+}
+
+else
+{
+   (c > b ); 
+
+   max = c;
+}
+
+ Console.WriteLine("max" );
