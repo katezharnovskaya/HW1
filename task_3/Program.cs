330ddfe2500cd5e36cc9e436f1e937b294211f90
Ekaterina <firegirl-83@mail.ru>
2023-01-22 07:21:42 +0300

    add task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+if((a % 2) == 0)
+{
+   Console.WriteLine("четное число");
+
+}
+else
+{
+
+Console.WriteLine("нечетное число");
+
+}
