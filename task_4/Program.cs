fc62bfa0b319a05d835c9931d5860c54565be546
Ekaterina <firegirl-83@mail.ru>
2023-01-23 07:20:15 +0300

    add task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+for (i < a)
+{
+    if (i % 2)
+
+    Console.WriteLine(a);
+    i++;
+
+};
+
+
+
